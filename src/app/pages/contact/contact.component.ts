eb00d3dc5b1c0b27376b5ce2f2f82de8ef67c25a
sivajisanapala <sivajis11@gmail.com>
2019-12-25 18:15:49 +0530

    2 files modified

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
+  
 
  routes = [
     {linkName: 'info1',url: 'info1'},
